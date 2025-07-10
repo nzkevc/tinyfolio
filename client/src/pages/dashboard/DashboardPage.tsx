c59dbd5df13e860621a733067e87da764e3f2e9d
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2025-07-10 16:30:09 +1200

    mock landing page (and centre dashboard page)

--- client/src/pages/dashboard/DashboardPage.tsx
+++ client/src/pages/dashboard/DashboardPage.tsx
@@ -1,8 +1,10 @@
+import CentredLayout from "@/components/CentredLayout";
+
 export default function DashboardPage() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
+    <CentredLayout>
       <h1 className="text-2xl font-bold">Dashboard Page</h1>
       <p className="mt-4 text-gray-600">This is the dashboard page.</p>
-    </div>
+    </CentredLayout>
   );
 }
