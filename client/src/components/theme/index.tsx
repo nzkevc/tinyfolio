ddfbc66b6d24d8dcb4bfeed7e2de781ebb9989bb
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2025-07-22 12:58:04 +1200

    add theme functionality on frontend

---
+++ client/src/components/theme/index.tsx
@@ -0,0 +1,4 @@
+import { ThemeProviderContext, type ThemeProviderState } from "./ThemeContext";
+import { ThemeProvider } from "./ThemeProvider.tsx";
+
+export { type ThemeProviderState, ThemeProviderContext, ThemeProvider };
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { useContext } from "react";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { ThemeProviderContext, type ThemeProviderState } from "@/components/theme";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function useTheme(): ThemeProviderState {
+  const context = useContext(ThemeProviderContext);
+
+  if (context === undefined) throw new Error("useTheme must be used within a ThemeProvider");
+
+  return context;
 }
