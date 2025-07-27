608150c38cb10c83a4bf0453654e666956af1c03
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2025-07-27 22:40:03 +1200

    add new cookie library which actually works

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,8 +1,14 @@
+import Cookies from "js-cookie";
 import { Link } from "react-router";
+
+import { getJwtSubject } from "@/lib/utils";
 
 import { ThemeModeToggle } from "./ThemeModeToggle";
 
 export default function TopBar() {
+  const cookies = Cookies.get();
+  const jwtSubject = getJwtSubject(cookies.ACCESS_TOKEN);
+
   return (
     <nav className="bg-primary-foreground flex items-center justify-between px-4 py-3">
       <div className="text-2xl font-bold italic">
@@ -11,8 +17,8 @@ export default function TopBar() {
         </Link>
       </div>
       <div className="flex items-center gap-4">
-        <Link to="/settings" className="">
-          Folio
+        <Link to={`/${jwtSubject}`} className="">
+          Your Folio
         </Link>
         <Link to="/dashboard" className="">
           Dashboard
