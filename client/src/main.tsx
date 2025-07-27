6a5873728ac0dc24438cc09238d8fca73eea4864
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2025-07-27 20:42:13 +1200

    add cookie provider to app to access cookies easily...

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "@/styles/globals.css";
 
+import { CookiesProvider } from "react-cookie";
 import { BrowserRouter } from "react-router";
 
 import App from "./App.tsx";
@@ -10,10 +11,12 @@ import { ThemeProvider } from "./components/theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <CookiesProvider defaultSetOptions={{ path: "/" }}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </CookiesProvider>
   </StrictMode>,
 );
